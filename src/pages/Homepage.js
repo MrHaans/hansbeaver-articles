14feab9faadc3a573d5edb5348aa8afc25f66f60
Hans Beaver <aansfarhans@gmail.com>
2023-03-15 03:08:34 +0700

    Add Button to My Website at Homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -13,7 +13,7 @@ export default function Homepage() {
         <h1 className="uppercase font-bold text-4xl tracking-wide mb-7 md:text-6xl lg:text-7xl items-center justify-center">
           Hans Beaver's Article
         </h1>
-        <button className="items-center justify-center">
+        <button className="items-center justify-center mb-8">
           <Link
             to="/blog"
             className="py-2 px-6 rounded shadow text-white bg-green-400 hover:bg-transparent border-2 border-green-400
@@ -21,6 +21,17 @@ export default function Homepage() {
           >
             Read my article post
           </Link>
+        </button>
+        <button className="items-center justify-center">
+          <a
+            href="https://hansbeaver-website.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="py-2 px-6 rounded shadow text-white bg-black hover:bg-transparent border-2 border-black
+            transition-all duration-500 hover:text-black font-bold"
+          >
+            Back to my website 
+          </a>
         </button>
       </section>
     </div>
