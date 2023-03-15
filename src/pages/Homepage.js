5466c6258d45ae85412af2d631478b04e2f15066
Hans Beaver <aansfarhans@gmail.com>
2023-03-15 20:29:34 +0700

    Adding Quote Animation in Homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,18 +1,56 @@
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import icon from "../images/icon.png";
 
 export default function Homepage() {
+  // NEW CODE FOR ANIMATION
+  const quotes = [
+    "Reading is an activity of the imagination, and the imagination in question is not the writer's alone. - THOMAS C. FOSTER",
+    "Do what you love, especially if your sharing the word. - ANN SIMPSON",
+    "I read to live, I write to share their life. - JESSIE WINTERSPRING",
+    "The more I learn, the more I realize how much I don’t know. - ALBERT EINSTEIN",
+    "Everyone who's born has a mysterious life path to follow. We must live to find that one reason to live. - SUCHET CHATURVEDI",
+  ];
+
+  const [quoteIndex, setQuoteIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setQuoteIndex((quoteIndex) =>
+        quoteIndex === quotes.length - 1 ? 0 : quoteIndex + 1
+      );
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, [quotes.length]);
+  // ANIMATION END
+
   return (
     <div>
       <section className="flex flex-col items-center justify-start text-center h-screen">
         <img
-          className="w-32 md:w-44 lg:w-52 mt-32 mb-8 items-center justify-center"
+          className="w-32 md:w-44 lg:w-52 mt-14 mb-8 items-center justify-center"
           src={icon}
           alt="Icon"
         />
-        <h1 className="uppercase font-bold text-4xl tracking-wide mb-10 md:text-6xl lg:text-7xl items-center justify-center">
+        <h1 className="uppercase font-bold text-4xl tracking-wide mb-0 md:text-6xl lg:text-7xl items-center justify-center">
           Hans Beaver's Article
         </h1>
+        {/* QUOTE ANIMATION CHANGER CODE  */}
+        <div className="quote-container">
+          {quotes.map((quote, index) => (
+            <div
+              key={index}
+              className={`quote-text ${
+                index === quoteIndex ? "opacity-100" : "opacity-0"
+              }`}
+            >
+              <p className="font-bold text-base md:text-lg lg:text-xl"><q>{quote}</q></p>
+            </div>
+          ))}
+        </div>
+        {/* ANIMATION END  */}
+
         <button className="items-center justify-center mb-10">
           <Link
             to="/blog"
@@ -30,7 +68,7 @@ export default function Homepage() {
             className="py-3 px-9 rounded shadow text-white bg-black hover:bg-transparent border-2 border-black
             transition-all duration-500 hover:text-black font-bold"
           >
-            Back to my website 
+            Back to my website
           </a>
         </button>
       </section>
